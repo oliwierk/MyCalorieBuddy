4b7229faa98ff4b651408e5da420b5e81a9e0370
oliwierk <oliwier.kotlicki@gmail.com>
2023-12-17 10:38:24 +0100

    db searching working - very high reads, app working

--- screens/FoodScreen.js
+++ screens/FoodScreen.js
@@ -1,50 +1,64 @@
-import { View, Text, FlatList } from "react-native";
 import React, { useState, useEffect } from "react";
-import { collection, query, where, onSnapshot } from "firebase/firestore";
-import { db2 } from "../MyCalBuddy";
+import { View, Text, FlatList, TextInput } from "react-native";
+import { collection, query, onSnapshot } from "firebase/firestore";
+import { db } from "../FirebaseConfig";
 import NavbarFilled from "../components/NavbarFilled";
-
 import tw from "twrnc";
 
 export default function FoodScreen() {
-	const [food, setFood] = useState([]);
+	const [allFood, setAllFood] = useState([]);
+	const [filteredFood, setFilteredFood] = useState([]);
 	const [loading, setLoading] = useState(true);
+	const [search, setSearch] = useState("");
 
 	useEffect(() => {
-		const foodQuery = query(
-			collection(db2, "products"),
-			where("name", "==", "Żurawina suszona - Finesia")
-		);
+		const foodQuery = query(collection(db, "products"));
 		const unsubscribe = onSnapshot(foodQuery, snapshot => {
 			const foodData = snapshot.docs.map(doc => ({
 				...doc.data(),
 				id: doc.id,
 			}));
-			setFood(foodData);
+			setAllFood(foodData);
+			setFilteredFood(foodData);
 			setLoading(false);
 		});
 
 		return () => unsubscribe();
 	}, []);
 
+	useEffect(() => {
+		const handler = setTimeout(() => {
+			const filteredData = allFood.filter(item =>
+				item.name.toLowerCase().includes(search.toLowerCase())
+			);
+			setFilteredFood(filteredData);
+		}, 500); // Opóźnienie 500ms
+
+		return () => {
+			clearTimeout(handler);
+		};
+	}, [search, allFood]);
+
 	const renderItem = ({ item }) => (
-		<View>
-			<Text style={tw`text-lg text-slate-50`}>{item.name}</Text>
-			<Text style={tw`text-lg text-slate-50`}>Kalorie: {item.calories}</Text>
-			<Text style={tw`text-lg text-slate-50`}>Węglowodane: {item.carbs}</Text>
-			<Text style={tw`text-lg text-slate-50`}>Tłuszcze: {item.fats}</Text>
-			<Text style={tw`text-lg text-slate-50`}>Białko: {item.protein}</Text>
+		<View style={tw`p-4 border-b border-gray-200`}>
+			<Text style={tw`text-lg text-white`}>{item.name}</Text>
+			<Text style={tw`text-sm text-gray-400`}>Kalorie: {item.calories}</Text>
+			<Text style={tw`text-sm text-gray-400`}>Węglowodany: {item.carbs}</Text>
+			<Text style={tw`text-sm text-gray-400`}>Tłuszcze: {item.fats}</Text>
+			<Text style={tw`text-sm text-gray-400`}>Białko: {item.protein}</Text>
 		</View>
 	);
-	if (loading) {
-		return <Text>Loading...</Text>;
-	}
 
 	return (
 		<View style={tw`h-full w-full bg-gray-900 flex flex-col gap-2`}>
 			<NavbarFilled />
+			<TextInput
+				placeholder='Wyszukaj jedzenie...'
+				onChangeText={text => setSearch(text)}
+				style={tw`text-lg bg-white p-2`}
+			/>
 			<FlatList
-				data={food}
+				data={filteredFood}
 				renderItem={renderItem}
 				keyExtractor={item => item.id}
 			/>
